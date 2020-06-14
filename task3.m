a58b80ecdc78658b7354aa879c4fc6d8993b9fb0
maham-patel <54802779+maham-patel@users.noreply.github.com>
2020-06-14 20:44:36 +0500

    Update task3.m

--- task3.m
+++ task3.m
@@ -1,20 +1,30 @@
-function task3(steps, startx, starty)
+function task3(steps, startx, starty, prob_nostep, prob_halfstep, prob_left, prob_right, prob_down)
     num = steps;
     sumx = startx;
     sumy = starty;
     plottx(1) = startx;
     plotty(1) = starty;
-    step_direction = randi([1,4],1,num);
+    %step_direction = randi([1,4],1,num);
+    step_direction = rand(num, 1);
+    step_size = rand(num, 1);
+    step = 0;
     
     for i = 2:num
-        if step_direction(i) == 1
-            sumx = sumx - 1;
-        elseif step_direction(i) == 2
-            sumx = sumx + 1;
-        elseif step_direction(i) == 3
-            sumy = sumy - 1;
+        if step_size(i) < prob_nostep
+            step = 0;
+        elseif step_size(i) < prob_nostep + prob_halfstep
+            step = 0.5;
         else
-            sumy = sumy + 1;
+            step = 1;
+        end
+        if step_direction(i) < prob_left
+            sumx = sumx - step;
+        elseif step_direction(i) < prob_left + prob_right
+            sumx = sumx + step;
+        elseif step_direction(i) < prob_left + prob_right + prob_down
+            sumy = sumy - step;
+        else
+            sumy = sumy + step;
         end
         plottx(i) = sumx;
         plotty(i) = sumy;
