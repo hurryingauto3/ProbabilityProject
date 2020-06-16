4b6e076cc755b910b2f331e0cc10f5c544a20a03
Ali Hamza <ah05084@st.habib.edu.pk>
2020-06-16 15:14:40 +0500

    Updated Task 5 with Polar

--- task5.m
+++ task5.m
@@ -1,37 +1,52 @@
-function task5(steps, startx, starty, prob_left, prob_right, prob_down)
+function task5(steps, startx, starty)
     num = steps;
     sumx = startx;
     sumy = starty;
+    
     plottx(1) = startx;
     plotty(1) = starty;
-    %step_direction = randi([1,4],1,num);
-    step_direction = rand(num, 1);
-    step_size = rand(num, 1);
+    
+    
+    %step_direction = randi([1,4],1,num)
+    R_values = rand(num, 1);
+    Theta_values = 2*pi.*rand(num,1);
     
     for i = 2:num
-        if step_direction(i) < prob_left
-            sumx = sumx - step_size(i);
-            if ((sumx-startx)^2 + (sumy-starty)^2)^(1/2) > 100
-                sumx = sumx + 2*step_size(i);
-            end
-        elseif step_direction(i) < prob_left + prob_right
-            sumx = sumx + step_size(i);
-            if ((sumx-startx)^2 + (sumy-starty)^2)^(1/2) > 100
-                sumx = sumx - 2*step_size(i);
-            end
-        elseif step_direction(i) < prob_left + prob_right + prob_down
-            sumy = sumy - step_size(i);
-            if ((sumx-startx)^2 + (sumy-starty)^2)^(1/2) > 100
-                sumy = sumy + 2*step_size(i);
-            end
-        else
-            sumy = sumy + step_size(i);
-            if ((sumx-startx)^2 + (sumy-starty)^2)^(1/2) > 100
-                sumy = sumy - 2*step_size(i);
-            end
+        if (sumx^2+sumy^2)^(1/2) > 100
+            sumx = sumx + R_values(i)*cos(Theta_values(i)+pi);
+            sumy = sumy + R_values(i)*sin(Theta_values(i)+pi);
         end
-        plottx(i) = sumx;
-        plotty(i) = sumy;
+        sumx = sumx + R_values(i)*cos(Theta_values(i));
+        sumy = sumy + R_values(i)*sin(Theta_values(i));
+        
+%         if step_direction(i) < prob_left
+%             sumx = sumx - step_size(i);
+%             sumr = sumr - (step_size(i));
+%             sumtheta = sumtheta + tan(
+%             if ((sumx-startx)^2 + (sumy-starty)^2)^(1/2) > 100
+%                 sumx = sumx + 2*step_size(i);
+%             end
+%         elseif step_direction(i) < prob_left + prob_right
+%             sumx = sumx + step_size(i);
+%             if ((sumx-startx)^2 + (sumy-starty)^2)^(1/2) > 100
+%                 sumx = sumx - 2*step_size(i);
+%             end
+%         elseif step_direction(i) < prob_left + prob_right + prob_down
+%             sumy = sumy - step_size(i);
+%             if ((sumx-startx)^2 + (sumy-starty)^2)^(1/2) > 100
+%                 sumy = sumy + 2*step_size(i);d
+%             end
+%         else
+%             sumy = sumy + step_size(i);
+%             if ((sumx-startx)^2 + (sumy-starty)^2)^(1/2) > 100
+%                 sumy = sumy - 2*step_size(i);
+%             end
+%         end
+        
+         plottx(i) = sumx;
+         plotty(i) = sumy;
+        
+
     end
     
     figure (1);
