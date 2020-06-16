1db44b6b1a76a8179fdebfda3b9732a6943bfc61
maham-patel <54802779+maham-patel@users.noreply.github.com>
2020-06-16 12:30:18 +0500

    Create task4.m

---
+++ task4.m
@@ -0,0 +1,23 @@
+function plott = task4(steps, start, right_prob, left_prob)
+    num = steps; %number of steps
+    sum = start; %starting position of the person
+    plott(1) = start; %the steps-position plot
+    step_direction = rand(num, 1); %random number which decides his movement
+    step_size = rand(num, 1);
+    
+    for i = 2:num
+        if step_direction(i) < left_prob
+            sum = sum - step_size(i);
+        else
+            sum = sum + step_size(i);
+        end
+        plott(i) = sum;
+    end
+    
+    figure (1);
+    c = plot(plott, 1:num, '-rx');
+    set(c, 'color', 'blue');
+    grid on;
+    title('random walk in 1 dimension')
+    ylabel('steps');
+end
