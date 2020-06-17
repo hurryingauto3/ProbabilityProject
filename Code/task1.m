9a1d033820e71f980959ca26ca4a05726ec3dc7a
Ali Hamza <ah05084@st.habib.edu.pk>
2020-06-17 22:30:59 +0500

    Task1changed, report started

--- Code/task1.m
+++ Code/task1.m
@@ -1,23 +1,25 @@
-function plott = task1(num_tries, start, right_prob, left_prob)
+function plott = task1(num_tries, start)
     num = num_tries;
     sum = start;
     plott(1) = start;
-    step_direction = rand(num, 1);
+    step_direction = randi([1,3],num,1);
     
     for i = 2:num
-        if step_direction(i) < left_prob
+        if step_direction(i) == 1
+            sum = sum + 1;
+        elseif step_direction(i) == 2
             sum = sum - 1;
         else
-            sum = sum + 1;
+            sum = sum + 0;
         end
-            plott(i) = sum;
+        plott(i) = sum;
     end
     
     figure (1);
     c = plot(plott, 1:num, '-rx');
     set(c, 'color', 'blue');
-    grid on;
-    title('random walk in 1 dimension')
+    grid off;
+    title('Random Walk in 1 Dimension')
     ylabel('steps');
 end
 
