f88378b7164e1cda782e74b53c00a1009c7c90cc
Ali Hamza <ah05084@st.habib.edu.pk>
2020-06-17 12:39:05 +0500

    Renamed files

---
+++ task1.m
@@ -0,0 +1,24 @@
+function plott = task1(num_tries, start, right_prob, left_prob)
+    num = num_tries;
+    sum = start;
+    plott(1) = start;
+    step_direction = rand(num, 1);
+    
+    for i = 2:num
+        if step_direction(i) < left_prob
+            sum = sum - 1;
+        else
+            sum = sum + 1;
+        end
+            plott(i) = sum;
+    end
+    
+    figure (1);
+    c = plot(plott, 1:num, '-rx');
+    set(c, 'color', 'blue');
+    grid on;
+    title('random walk in 1 dimension')
+    ylabel('steps');
+end
+
+
