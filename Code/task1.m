a29372cddf6bd815bd4ee3caf7900722b40487d0
maham-patel <54802779+maham-patel@users.noreply.github.com>
2020-06-18 19:13:11 +0500

    Update task1.m

--- Code/task1.m
+++ Code/task1.m
@@ -1,25 +1,24 @@
-function sum = task1(num_tries, start)
-    num = num_tries;
-    sum = start;
-    plott(1) = start;
-    step_direction = randi([1,3],num,1);
+function plott = task1(steps, start, left_prob, no_step_prob)
+    num = steps; %number of steps
+    sum = start; %starting position of the person
+    plott(1) = start; %the steps-position plot
+    step_direction = rand(num, 1); %random number which decides his movement
     
     for i = 2:num
-        if step_direction(i) == 1
-            sum = sum + 1;
-        elseif step_direction(i) == 2
+        if step_direction(i) < no_step_prob
+            sum = sum;
+        elseif step_direction(i) < no_step_prob + left_prob
             sum = sum - 1;
         else
-            sum = sum + 0;
+            sum = sum + 1;
         end
         plott(i) = sum;
     end
     
 %     figure (1);
-%     c = plot(plott, 1:num, '-rx');
+%     c = plot(1:num, plott, '-rx');
 %     set(c, 'color', 'blue');
-%     grid off;
-%     title('Random Walk in 1 Dimension')
+%     grid on;
+%     title('random walk in 1 dimension')
 %     ylabel('steps');
-
 end
