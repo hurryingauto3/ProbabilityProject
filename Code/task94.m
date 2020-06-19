752f0dd9a13104e0085a49059053d50ea818ac88
hn05012 <49056735+hn05012@users.noreply.github.com>
2020-06-19 15:58:10 +0500

    Updated task94.m

--- Code/task94.m
+++ Code/task94.m
@@ -17,11 +17,10 @@ function avg_step = task93(sim, steps, a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y)
     step_arr = zeros(1, steps);
     % mean_step_arr = zeros(1, nums);
     
-    
 
     for i = 1:nums
         for j = 1:steps
-            if ((particle4(1,j,i)-particle3(1,j,i))^2 +(particle4(2,j,i)-particle3(2,j,i)^2))^(1/2) > 6 && ((particle4(1,j,i)-particle2(1,j,i))^2 +(particle4(2,j,i)-particle2(2,j,i)^2))^(1/2) > 6 && ((particle4(1,j,i)-particle1(1,j,i))^2 +(particle4(2,j,i)-particle1(2,j,i)^2))^(1/2) > 6 && ((particle3(1,j,i)-particle2(1,j,i))^2 +(particle3(2,j,i)-particle2(2,j,i)^2))^(1/2) > 6 && ((particle3(1,j,i)-particle1(1,j,i))^2 +(particle3(2,j,i)-particle1(2,j,i)^2))^(1/2) > 6 && ((particle2(1,j,i)-particle1(1,j,i))^2 +(particle2(2,j,i)-particle1(2,j,i)^2))^(1/2) > 6  
+            if ((particle4(1,j,i)-particle3(1,j,i))^2 +(particle4(2,j,i)-particle3(2,j,i)^2))^(1/2) <= 6 && ((particle4(1,j,i)-particle2(1,j,i))^2 +(particle4(2,j,i)-particle2(2,j,i)^2))^(1/2) <= 6 && ((particle4(1,j,i)-particle1(1,j,i))^2 +(particle4(2,j,i)-particle1(2,j,i)^2))^(1/2) <= 6 && ((particle3(1,j,i)-particle2(1,j,i))^2 +(particle3(2,j,i)-particle2(2,j,i)^2))^(1/2) <= 6 && ((particle3(1,j,i)-particle1(1,j,i))^2 +(particle3(2,j,i)-particle1(2,j,i)^2))^(1/2) <= 6 && ((particle2(1,j,i)-particle1(1,j,i))^2 +(particle2(2,j,i)-particle1(2,j,i)^2))^(1/2) <= 6  
                 step_arr(i) = j;
             end
         end
@@ -29,6 +28,6 @@ function avg_step = task93(sim, steps, a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y)
     end
     
 
-avg_step = mean(step_arr, 'all');
+avg_step = mean(step_arr, 'all');   
 
 end
